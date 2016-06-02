f590fa0f8288f7c9a5139eef768188bfefe8450e
Juan Muga <juanmuga@Juan-Mugas-MBP.fibertel.com.ar>
2016-06-02 15:38:02 -0300

    agregado files viejos de flo para adaptar

---
+++ Static/FloArte.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+
+     $('.slider').slider();
+
+      
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+
+
+    
+
+    
+
+
+  });
+	
+
+   
+       
